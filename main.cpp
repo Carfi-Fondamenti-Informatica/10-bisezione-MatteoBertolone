54ea7a3b92be5317fd8bd9bdd5cd0c842b4f3429
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-12-19 22:04:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@ float f(float y){
     return pow(y,2)*cos(y)+1;
 }
 
-void bisez(float a, float b){
-    float x=0,err=0;
+int main() {
+    float a=0,b=0, x=0,err=0;
+    do {
+        cout << "inserire estremi"<<endl;
+        cin >> a;
+        cin >> b;
+    }while(f(a)*f(b)>=0);
     do {
         x=(a+b)/2;
         if(f(x)!=0) {
@@ -20,15 +25,6 @@ void bisez(float a, float b){
         err=abs((b-a)/2);  
     }while(err>=1e-6);
     cout << int(x * 10000) / 10000.0 <<endl;
-}
-
-int main() {
-    float a=0,b=0;
-    do {
-        cout << "inserire estremi"<<endl;
-        cin >> a;
-        cin >> b;
-    }while(f(a)*f(b)>=0);
-    bisez(a,b);
+    cout << int(f(x) * 10000) / 10000.0 <<endl;
     return 0;
 }
